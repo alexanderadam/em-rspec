25e580402a72f7490a3184d01ff9bcb15577f363
leizzer <lizzydw@gmail.com>
2014-06-05 11:45:01 -0300

    wrapping tests for eventmachine

--- lib/patch.rb
+++ lib/patch.rb
@@ -10,15 +10,22 @@ RSpec::Core::Example.class_eval do
   alias ignorant_run run
 
   def run(example_group_instance, reporter)
-    EM.run do
-      Fiber.new do
-        df = EM::DefaultDeferrable.new
-        df.callback { |x| EM.stop }
 
-        ignorant_run example_group_instance, reporter
+    if metadata[:eventmachine]
 
-        df.succeed
-      end.resume
+      EM.run do
+        Fiber.new do
+          df = EM::DefaultDeferrable.new
+          df.callback { |x| EM.stop }
+  
+          ignorant_run example_group_instance, reporter
+  
+          df.succeed
+        end.resume
+      end
+
+    else
+      ignorant_run example_group_instance, reporter
     end
   end
 
